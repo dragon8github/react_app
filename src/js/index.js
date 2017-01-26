e08aef1c0e2780c0fce7cd8a246258447bddf895
dragon8github <928532756@qq.com>
2017-01-26 20:14:42 +0800

    优化

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,82 @@
-/* 加载React系列 */
-var React = require("react")
-var ReactDOM = require("react-dom")
-import { Router, Route, hashHistory } from 'react-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-/* 加载组件 */
-import { Mysearch } from './../components/header.js'
-import { Mytable } from './../components/table.js'
-/* 加载CSS */
-require('./../assets/css/style.css')
-require('./../assets/css/cont.css')
-require('./../assets/css/eTong.Layer1.0.css')
-   
+import 'antd/dist/antd.css'; 
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
 
-var App = React.createClass({
-	render () {
-		return (
-			<div>
-				<Mysearch />,
-   				<Mytable />
-			</div>
-		)
-	}
-})
 
-ReactDOM.render( 
-    <App />,
-    document.getElementById('app') 
-)     
+class SiderDemo extends React.Component {
+    //官方原码
+    state = {
+       collapsed: false
+    };
+    // //自己尝试
+    // getInitialState=()=>{
+    //     return {
+    //         collapsed:false
+    //     }
+    // }
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({ collapsed });
+    }
+    render() {
+        return (
+            <Layout>
+                <Sider
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}
+                >
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span className="nav-text">nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span className="nav-text">nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span className="nav-text">nav 3</span>
+                        </Menu.Item>
+                        <Menu.Item key="4">
+                            <Icon type="user" />
+                            <span className="nav-text">nav 4</span>
+                        </Menu.Item>
+                        <Menu.Item key="5">
+                            <Icon type="heart-o" />
+                            <span className="nav-text">nav 5</span>
+                        </Menu.Item>
+                        <Menu.Item key="6">
+                            <Icon type="team" />
+                            <span className="nav-text">nav 6</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }} />
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '12px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                            content
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design 2016 Created by Ant UED
+                    </Footer>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
+
+ReactDOM.render(<SiderDemo />, document.getElementById('app'));
